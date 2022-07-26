6aef6c19c83d462a0467e05a43a23609f8b36b81
Egor-oop <gulidoegor@gmail.com>
2022-07-26 13:54:36 +0300

    [feature] added model for Order

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from dishes.models import Dish
+
+
+class Order(models.Model):
+    dishes = models.ManyToManyField(Dish,
+                                    verbose_name='Блюда')
+    created_at = models.DateTimeField(auto_now_add=True,)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+# class OrderDishes(models.Model):
+#     order = models.ForeignKey(Order, on_delete=models.CASCADE,
+#                               verbose_name='Заказ')
+#     dish = models.ForeignKey(Dish, on_delete=models.CASCADE,
+#                              verbose_name='Блюдо')
+#     quantity = models.IntegerField(verbose_name='Количество')
