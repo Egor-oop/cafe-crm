2f360ce056bae50941f60be853097a82311d3170
Egor-oop <gulidoegor@gmail.com>
2022-07-26 13:04:07 +0300

    [feature] created model for dishes app

--- backend/dishes/models.py
+++ backend/dishes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(max_length=64,
+                            verbose_name='Название')
+    price = models.FloatField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
