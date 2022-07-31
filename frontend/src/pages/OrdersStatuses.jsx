93a48a3be5821acc626865cd390782cd65ebbf20
Egor-oop <gulidoegor@gmail.com>
2022-07-31 17:58:33 +0300

    [fix & feature] fixed loop and added router

---
+++ frontend/src/pages/OrdersStatuses.jsx
@@ -0,0 +1,41 @@
+import { UsersOrderView } from '../components/UsersOrderView';
+
+import { useState, useEffect } from 'react';
+
+export const OrdersStatuses = () => {
+  const [orders, setOrder] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/v1/orders/', {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      }
+    })
+      .then((response) => response.json())
+      .then((actualData) => (setOrder(actualData) & console.log(actualData)));
+  }, [orders.is_done]);
+
+  return (
+    <div className='main'>
+      <div className='orders__not-done'>
+        <h2 className='orders__not-done-header'>Готовятся</h2>
+
+        {orders.map(order => (
+          !order.is_done ?
+            <UsersOrderView key={order.id} orderId={order.id} />
+            : ''
+        ))}
+      </div>
+      <div className='orders__done'>
+        <h2 className='orders__done-header'>Готовы</h2>
+
+        {orders.map(order => (
+          order.is_done ?
+            <UsersOrderView key={order.id} orderId={order.id} />
+            : ''
+        ))}
+      </div>
+    </div>
+  );
+}
