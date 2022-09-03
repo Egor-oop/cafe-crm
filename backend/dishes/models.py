70c76381c452849da7919375a1525b6f3f05e75d
Egor-oop <gulidoegor@gmail.com>
2022-09-03 13:49:04 +0300

    hotfix: added verbose name to dish price

--- backend/dishes/models.py
+++ backend/dishes/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Dish(models.Model):
     name = models.CharField(max_length=64,
                             verbose_name='Название')
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
